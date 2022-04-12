c46a73008a8c729a7fe24fb7decb6a025abf8ea0
Abhishek Raj <abhishekraj1130@gmail.com>
2022-04-12 22:34:32 +0530

    mong db connection established

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express  from 'express'
 import cors from 'cors'
+import connectDb from './config/connectdb.js'
 
 const app = express()
 const port = process.env.PORT
 
+const DATABASE_URL = process.env.DATABASE_URL 
+
+connectDb(DATABASE_URL)
+
 app.use(cors())
+
+app.use(express.json())
 
 app.listen(port, () => {
     console.log(`Server listening at htttp://localhost:${port}`)
 })
+
