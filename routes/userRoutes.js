f2cd13d68ce9732e3aae9ee66cf49eb05fb8b26a
Abhishek Raj <abhishekraj1130@gmail.com>
2022-04-17 18:33:54 +0530

    change password by jwt token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 const router = express.Router();
 import UserController from '../controllers/userController.js';
+import checkUserAuth from "../middlewares/auth.js";
+
+//route level middleware - to protect route
+router.use('/changepassword',checkUserAuth)
 
 //public routes
 router.post('/register',UserController.userRegisteration)
 router.post('/login',UserController.userLogin)
 
 //protected routes
-
+router.post('/changepassword',UserController.changePassword)
 
 export default router;
